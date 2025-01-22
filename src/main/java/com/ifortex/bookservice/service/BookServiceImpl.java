1499de254e2734d9da5069a50a9bc08a521860f5
egor <egoisntegor@gmail.com>
2025-01-22 22:08:45 +0300

    Classes for implementation

---
+++ src/main/java/com/ifortex/bookservice/service/BookServiceImpl.java
@@ -0,0 +1,10 @@
+package com.ifortex.bookservice.service;
+
+import java.util.Map;
+
+public class BookServiceImpl implements BookService {
+    @Override
+    public Map<String, Long> getBooks() {
+        return Map.of();
+    }
+}
---
+++ src/main/java/com/ifortex/bookservice/service/MemberServiceImpl.java
@@ -0,0 +1,17 @@
+package com.ifortex.bookservice.service;
+
+import com.ifortex.bookservice.model.Member;
+
+import java.util.List;
+
+public class MemberServiceImpl implements MemberService{
+    @Override
+    public Member findMember() {
+        return null;
+    }
+
+    @Override
+    public List<Member> findMembers() {
+        return List.of();
+    }
+}
